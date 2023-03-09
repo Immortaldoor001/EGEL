bfcd13d27457a4b7e9704acc9d4a5ff87a828e45
UrielJM <62513979+Immortaldoor001@users.noreply.github.com>
2023-03-09 14:59:37 -0600

    Avances dia 2

---
+++ 1. Algoritmia/1.1 Analisis y diseño de algoritmos/Codigos y programas/recursividas_jv.java
@@ -0,0 +1,17 @@
+class recursividad {
+    public static void main(String args[]) {
+
+        System.out.println(fibonacci(10));
+
+    }
+
+    static int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+}
